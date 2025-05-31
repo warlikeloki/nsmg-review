45354b5889542f51c0d449d7ebc610f3b4cf3387
warlikeloki <neil.allen.smith@gmail.com>
2025-05-31 19:44:19 -0400

    adding automated testing

---
+++ cypress/plugins/index.js
@@ -0,0 +1,7 @@
+// cypress/plugins/index.js
+// Cypress will call this function when it launches. You can hook into task events here.
+// For now, just return the config unchanged.
+
+module.exports = (on, config) => {
+  return config;
+};
